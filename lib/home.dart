91e25899d5fccf523458ecd443cc020addb1c5ea
MohamedEbrahimRezq <164947638+MohamedEbrahimRezq@users.noreply.github.com>
2025-01-16 01:21:29 +0200

    Api classes and news Tab controller

---
+++ lib/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/app_utls/app_colors.dart';
+
+class HomePage extends StatelessWidget {
+  static const String routeName = 'home_page';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: AppColors.blue,
+
+      ),
+    );
+  }
+}
